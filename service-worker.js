24484a42a1ce9df7495238c562f0e8bdf9208ff5
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2025-10-31 11:14:30 +0100

    Added coloring depending on %

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,18 @@ chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => 
 		let i = parseInt(message.data);
 		if(i >= 0 && i < 100)
 		{
-			chrome.action.setBadgeBackgroundColor({ color: 'green' });
+			if(i < 35)
+				chrome.action.setBadgeBackgroundColor({ color: 'green' });
+			else if(i < 50)
+				chrome.action.setBadgeBackgroundColor({ color: 'orange' });
+			else if(i < 55)
+				chrome.action.setBadgeBackgroundColor({ color: 'red' });
+			else if(i < 85)
+				chrome.action.setBadgeBackgroundColor({ color: 'green' });
+			else if(i < 100)
+				chrome.action.setBadgeBackgroundColor({ color: 'orange' });
+			else
+				chrome.action.setBadgeBackgroundColor({ color: 'red' });
 			chrome.action.setBadgeText({ text: message.data + "%" });
 		}
 	}
